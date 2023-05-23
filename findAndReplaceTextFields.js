f40b96a6b166434f5860445cf45cedb38011a292
Lee-250 <lee_b250@hotmail.co.uk>
2023-05-23 11:10:58 +0100

    update file names in findAndReplaceTextFields

--- findAndReplaceTextFields.js
+++ findAndReplaceTextFields.js
@@ -3,14 +3,14 @@ const JSZip = require("jszip");
 const PizZip = require("pizzip");
 
 // Read the DOCX file
-const content = fs.readFileSync("./ExampleDOCX/no-text-fields.docx", "binary");
+const content = fs.readFileSync("./ExampleDOCX/text-field-example.docx", "binary");
 
 // Load the content into a Zip object
 const zip = new PizZip(content);
 
 // Extract the XML file from the Zip object
 const xmlContent = zip.file("word/document.xml").asText();
-fs.writeFileSync('./Output/NO-TEXT-FIELDS-EXAMPLE.xml', xmlContent);
+fs.writeFileSync('./Output/TEXT-FIELDS-BEFORE.xml', xmlContent);
 
 const nameSearch = '<w:t>Name</w:t>';
 const nameReplace = '<w:t>Mr John Smith</w:t>';
@@ -20,7 +20,7 @@ const senderReplace = '<w:t>DVLA</w:t>';
 const updatedXmlContentWithName = xmlContent.replace(nameSearch, nameReplace);
 const updatedXmlContentWithSender = updatedXmlContentWithName.replace(senderSearch, senderReplace);
 
-fs.writeFileSync('./Output/TEXT-FIELD.xml', updatedXmlContentWithSender);
+fs.writeFileSync('./Output/TEXT-FIELDS-AFTER.xml', updatedXmlContentWithSender);
 
 // Convert the modified XML back to a buffer
 const updatedXmlBuffer = Buffer.from(updatedXmlContentWithSender, "utf-8");
@@ -32,4 +32,4 @@ zip.file("word/document.xml", updatedXmlBuffer);
 const updatedContent = zip.generate({ type: "nodebuffer" });
 
 // Save the updated DOCX file
-fs.writeFileSync("./Output/FIND_AND_REPLACE.docx", updatedContent);
+fs.writeFileSync("./Output/REPLACED_TEXT_FIELDS.docx", updatedContent);
